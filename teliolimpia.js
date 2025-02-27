aea2d09ae8aee6fb41b0e1a657466359edcc4a94
marton251 <hohmar748@hengersor.hu>
2025-02-27 09:15:51 +0100

    GET hiba javitása

--- teliolimpia.js
+++ teliolimpia.js
@@ -5,6 +5,11 @@ const bodyparser=require("body-parser")
 
 app.use(bodyparser.json())
 
+app.get("/",(req,res) =>{
+    res.send("müködik a szerver.")
+})
+
+
 app.listen(3000, () => {
     console.log("A téli olimpia szervere a 3000 porton fut")
 })
